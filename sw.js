a51ed48b19ec347a7076ae3b34dc6dee744644ff
dag1264 <galindo_diego@hotmail.com>
2026-03-04 11:18:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = "padel-random-v1";
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('padel-app').then(function(cache) {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        './',
-        './manifest.json'
+        "/padel-random-pro/",
+        "/padel-random-pro/index.html",
+        "/padel-random-pro/manifest.json"
       ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
